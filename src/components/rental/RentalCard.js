5fec99d5098c90b9626fb2d597e59d02a4f8ce95
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-22 23:17:35 +0100

    style(): text color, text decoration

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -6,11 +6,11 @@ export function RentalCard(props) {
 
   return (
     <div className="col-md-3 col-xs-6">
-      <Link to={`/rentals/${rental.id}`}>
+      <Link className="rental-detail-link" to={`/rentals/${rental.id}`}>
         <div className="card bwm-card">
           <img className="card-img-top" src={rental.image} alt=""></img>
           <div className="card-block">
-            <h6 className="card-subtitle">{rental.shared ? 'Shared' : 'Whole'} {rental.category} &#183; {rental.city}</h6>
+            <h6 className={`card-subtitle ${rental.category}`}>{rental.shared ? 'Shared' : 'Whole'} {rental.category} &#183; {rental.city}</h6>
             <h4 className="card-title">{rental.title}</h4>
             <p className="card-text">{rental.dailyRate} per Night &#183; Free Cancellation</p>
           </div>
