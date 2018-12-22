b5ee9b269fb536c7e94583f48e8b640f0cd09aba
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-22 23:38:02 +0100

    restructure(rental folder), add(env)

--- src/components/rental/rental-listing/RentalCard.js
+++ src/components/rental/rental-listing/RentalCard.js
@@ -8,7 +8,7 @@ export function RentalCard(props) {
     <div className="col-md-3 col-xs-6">
       <Link className="rental-detail-link" to={`/rentals/${rental.id}`}>
         <div className="card bwm-card">
-          <img className="card-img-top" src={rental.image} alt=""></img>
+          <img className="card-img-top" src={rental.image} alt="rental main"></img>
           <div className="card-block">
             <h6 className={`card-subtitle ${rental.category}`}>{rental.shared ? 'Shared' : 'Whole'} {rental.category} &#183; {rental.city}</h6>
             <h4 className="card-title">{rental.title}</h4>
