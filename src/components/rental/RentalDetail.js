4f9a047d092de817c33c1c1fe13f37ff13e05522
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-22 15:18:06 +0100

    feat(app): redux store

--- src/components/rental/RentalDetail.js
+++ src/components/rental/RentalDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 export class RentalDetail extends React.Component {
 
   render() {
+    console.log(this.props.match.params.id);
     return(
       <p>RentalDetailComponent</p>
       )
