d3bab0e78c820d7b33e5a1c6e9dcde956fbe00e8
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-22 16:47:02 +0100

    refactor & feat(rentalsReducer): handle the state of rentals

--- src/components/rental/RentalDetail.js
+++ src/components/rental/RentalDetail.js
@@ -3,7 +3,6 @@ import React from 'react';
 export class RentalDetail extends React.Component {
 
   render() {
-    console.log(this.props.match.params.id);
     return(
       <p>RentalDetailComponent</p>
       )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import * as redux from 'redux';
+import { rentalReducer } from './rental-reducer';
 
 export const init = () => {
   const reducer = redux.combineReducers({
-
+    rentals: rentalReducer
   });
 
   const store = redux.createStore(reducer);
