4066c3b72c3043d2c8889af97c1c52fb78992c0a
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-22 12:34:03 +0100

    feat(RentalCard): rental listing element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Header } from './shared/Header';
 import './App.css';
+import { RentalCard } from './components/rental/RentalCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <RentalCard />
       </div>
     );
   }
