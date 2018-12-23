7f74ff24843d8e415c69ff7c5da637ed63d7bf67
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-23 23:17:27 +0100

    feat(withGeoCode): apply cache

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Cacher } from 'services/cacher';
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker,
   Circle,
 } from "react-google-maps";
 
@@ -29,6 +29,7 @@ const withGeoCode = WrappedComponent => (
 
     constructor() {
       super();
+      this.cacher = new Cacher;
       this.state = {
         coordinates: {
           latitude: 0,
@@ -38,23 +39,32 @@ const withGeoCode = WrappedComponent => (
     }
 
     componentWillMount() {
-      this.geoCodeLocation();
+      this.getGeocodedLocation();
     }
 
-    geoCodeLocation() {
+    getGeocodedLocation() {
       const location = this.props.location;
       const geocoder = new window.google.maps.Geocoder();
 
-      geocoder.geocode({address: location}, (result, status) => {
-        if (status === 'OK') {
-          const geometry = result[0].geometry.location;
-          const coordinates = { lat: geometry.lat(), lng: geometry.lng() };
+      if (this.cacher.isValueCached(location)) {
+        this.setState({
+          coordinates: this.caches.getCachedValue(location)
+        });    
+      } else {
+        geocoder.geocode({address: location}, (result, status) => {
+          if (status === 'OK') {
+            const geometry = result[0].geometry.location;
+            const coordinates = { lat: geometry.lat(), lng: geometry.lng() };
+            
+            this.cacher.cacheValue(location, coordinates);
 
-          this.setState({
-            coordinates
-          })
-        }
-      });
+            this.setState({
+              coordinates
+            });
+          }
+        });
+      }
+
     }
 
     render() {
