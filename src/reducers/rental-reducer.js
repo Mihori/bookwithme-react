6be2e4a7585963f4f62ca390962f7596cfa077e4
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-23 14:31:37 +0100

    refactor(rental-reducer.js): remove unused variables, cases

--- src/reducers/rental-reducer.js
+++ src/reducers/rental-reducer.js
@@ -1,4 +1,4 @@
-import { GET_RENTALS, GET_RENTAL_BY_ID, RESET_RENTAL, RENDER_RENTAL, RENDER_RENTALS } from "../actions/types";
+import { RESET_RENTAL, RENDER_RENTAL, RENDER_RENTALS } from "../actions/types";
 
 const INITIAL_STATE = {
   rentals: {
@@ -24,8 +24,6 @@ export const selectedRentalReducer = (state = INITIAL_STATE.rental, action) => {
     return { ...state, data: {} };
     case RENDER_RENTAL: 
     return { ...state, data: action.rental }
-    case GET_RENTAL_BY_ID:
-      return Object.assign({}, state, { data: action.rental });
     default:
       return state;
   }
