859313308b8294150ca9cde5dbb7edf574a2dc2c
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-24 22:23:50 +0100

    feat(FormInput): handle form properties and errors

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,60 +1,43 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { FormInput } from '../../shared/form/FormInput';
 
 const RegisterForm = props => {
-  const { handleSubmit, pristine, reset, submitting, registerUser } = props;
+  const { handleSubmit, pristine, submitting, registerUser } = props;
 
   return (
     <form onSubmit={handleSubmit(registerUser)}>
-      <div>
-        <label>Username</label>
-        <div>
           <Field
             name="username"
-            component="input"
+            label="Username"
             type="text"
             className="form-control"
+            component={FormInput}
           />
-        </div>
-      </div>
-      <div>
-        <label>Email</label>
-        <div>
           <Field
             name="email"
-            component="input"
+            label="Email"
             type="email"
             className="form-control"
+            component={FormInput}
           />
-        </div>
-      </div>
-      <div>
-        <label>Password</label>
-        <div>
           <Field
             name="password"
-            component="input"
+            label="Password"
             type="password"
             className="form-control"
+            component={FormInput}
           />
-        </div>
-      </div>
-      <div>
-        <label>Confirmation Password</label>
-        <div>
           <Field
             name="passwordConfirmation"
-            component="input"
+            label="Confirmation Password"
             type="password"
             className="form-control"
+            component={FormInput}
           />
-        </div>
-      </div>
-      <div>
         <button className="btn btn-bwm btn-form" type="submit" disabled={pristine || submitting}>
           Submit
         </button>
-      </div>
     </form>
   )
 }
