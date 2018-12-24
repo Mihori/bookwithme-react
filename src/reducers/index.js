121eb1e9cbfc0ede6e3c904c963b567254f3e70a
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-24 14:45:01 +0100

    feat(reducer): add form reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { rentalReducer, selectedRentalReducer } from './rental-reducer';
 import thunk from 'redux-thunk';
 
+import { reducer as formReducer } from 'redux-form';
+
 export const init = () => {
   const reducer = combineReducers({
     rentals: rentalReducer,
     rental: selectedRentalReducer,
+    form: formReducer
   });
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
