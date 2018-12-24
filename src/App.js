77b1589182efc444ab6bdd2a1f6cd9a6eb0125f1
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-24 14:16:58 +0100

    feat(App): add route to Register

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import RentalListing from 'components/rental/rental-listing/RentalListing';
 import RentalDetail from 'components/rental/rental-detail/RentalDetail';
 
 import './App.css';
-import { Login } from './components/login/Login';
+import { Login } from 'components/login/Login';
+import { Register } from 'components/register/Register';
 
 const store = require('./reducers').init();
 
@@ -25,6 +26,7 @@ class App extends Component {
             <Route exact path="/rentals" component={RentalListing} />
             <Route exact path="/rentals/:id" component={RentalDetail} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
           </div>
         </div>
         </BrowserRouter>
