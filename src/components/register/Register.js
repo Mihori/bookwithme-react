34aa2ecba7fa0eb27ee776b4f2b2bb0406ec259e
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-24 14:22:28 +0100

    feat(Register): add form

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -3,6 +3,23 @@ import React from 'react';
 export class Register extends React.Component {
 
   render() {
-    return <h1>RegisterComponent</h1>
+    return (
+      <section id='register'>
+        <div className='bwm-form'>
+          <div className='row'>
+            <div className='col-md-5'>
+              <h1>Register</h1>
+              REGISTER FORM HERE!!!
+            </div>
+            <div className='col-md-6 ml-auto'>
+              <div className='image-container'>
+                <h2 className='catchphrase'>As our member you have access to most awesome places in the world.</h2>
+                <img src='' alt=""/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
   }
 }
