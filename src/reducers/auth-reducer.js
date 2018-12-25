8bae3a1ecee2b795b76cea69a6c26b27906b0837
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-25 09:27:23 +0100

    feat(authReducer): use login action cases

---
+++ src/reducers/auth-reducer.js
@@ -0,0 +1,18 @@
+import { LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions/types";
+
+const INITIAL_STATE = {
+  isAuth: false,
+  token: '',
+  errors: []
+}
+
+export const authReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case LOGIN_SUCCESS:    
+      return { ...state, isAuth: true, token: action.token, errors: [] };
+    case LOGIN_FAILURE:    
+      return { ...state, errors: action.errors };
+    default:
+      return state;
+  }
+}
