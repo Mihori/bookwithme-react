439367e396bcf569197e32e44fd1e45cb161ba93
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-25 12:04:31 +0100

    feat(AuthService): verify token validity

---
+++ src/services/auth-service.js
@@ -0,0 +1,27 @@
+import * as jwt from 'jsonwebtoken';
+import * as moment from 'moment';
+
+class AuthService {
+
+  getToken() {
+    return localStorage.getItem('auth_token');
+  }
+
+  getExpiration(token) {    
+    return moment.unix(jwt.decode(token).exp);
+  }
+
+  isValid(token) {
+    return moment().isBefore(this.getExpiration(token));
+  }
+
+  isAuthenticated() {
+    const token = this.getToken();
+
+    return token && this.isValid(token) ? true : false;
+  }
+
+}
+
+
+export default new AuthService();
