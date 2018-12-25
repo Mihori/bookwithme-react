6b7c2f803e279bb5d8f400cab9ec996a8487a458
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-25 17:25:40 +0100

    fix(AxiosService): add localhost to baseURL

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -11,7 +11,7 @@ class AxiosService {
 
   initInstance() {
     this.axiosInstance = axios.create({
-      baseURL: '/api/v1',
+      baseURL: 'http://localhost:3001/api/v1',
       timeout: 5000
     });
 
