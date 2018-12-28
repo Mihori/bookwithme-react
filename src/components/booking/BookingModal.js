648d5d1b18be615e3e5cc07c298d8b087ccfc5af
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-28 15:34:39 +0100

    feat(BookingModal): show proposed rental details

--- src/components/booking/BookingModal.js
+++ src/components/booking/BookingModal.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Modal from 'react-responsive-modal';
+import { ResponseError } from 'shared/form/ResponseError'
 
 export function BookingModal(props) {
-  const { open, closeModal } = props;
+  const { open, closeModal, booking, confirmModal, errors, rentalPrice } = props;
 
   return (
-    <Modal open={open} onClose={closeModal} >
-     <h4>Confirm Booking </h4>
+    <Modal open={open} onClose={closeModal} little classNames={{ modal: 'booking-modal' }}>
+     <h4 className='modal-title title'>Confirm Booking </h4>
+     <p className='dates'>{booking.startAt} / {booking.endAt}</p>
+     <div className='modal-body'>
+      <em>{booking.days}</em> nights /
+      <em>{rentalPrice}$</em> per Night
+      <p>Guests: <em>{booking.guests}</em></p>
+      <p>Price: <em>{booking.totalPrice}$ </em></p>
+      <p>Do you confirm your booking for selected days?</p>
+    </div>
+    <ResponseError errors={errors} />
+    <div className='modal-footer'>
+      <button onClick={confirmModal} type='button' className='btn btn-bwm'>Confirm</button>
+      <button type='button' onClick={closeModal} className='btn btn-bwm'>Cancel</button>
+    </div>
   </Modal>
   )
 }
