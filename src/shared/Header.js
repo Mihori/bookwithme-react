ea2cbb5e9eaed5b8f788a0fdc71c51d9a8d48553
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-30 21:14:10 +0100

    feat(Header): add owner section items

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -17,9 +17,7 @@ class Header extends React.Component {
     this.props.history.push('/rentals');
   }
 
-  renderAuthButtons() {
-    const { isAuth } = this.props.auth;
-
+  renderAuthButtons(isAuth) {
     if (isAuth) {
       return <a className="nav-item- nav-link clickable" onClick={this.handleLogout}>Logout</a>;
     }
@@ -31,7 +29,26 @@ class Header extends React.Component {
     )
   }
 
+  renderOwnerSection(isAuth) {
+    if (isAuth) {
+      return (
+        <div className="nav-item dropdown">
+          <a className="nav-link nav-item dropdown-toggle clickable" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Owner Section
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <Link className="dropdown-item" to="/rentals/new">Create Rental</Link>
+            <Link className="dropdown-item" to="/rentals/manage">Manage Rentals</Link>
+            <Link className="dropdown-item" to="/bookings/manage">Manage Bookings</Link>
+          </div>
+        </div>
+      )
+    }
+  }
+
   render() {
+    const { isAuth } = this.props.auth;
+
     return (
       <nav className="navbar navbar-dark navbar-expand-lg">
         <div className="container">
@@ -42,7 +59,8 @@ class Header extends React.Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ml-auto">
-              {this.renderAuthButtons()}
+              {this.renderOwnerSection(isAuth)}
+              {this.renderAuthButtons(isAuth)}
             </div>
           </div>
         </div>
