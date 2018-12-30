6bd944b5646b6af48508334892c513afda876986
Mihori <gabor.dobrozemszky@hotmail.com>
2018-12-30 13:08:34 +0100

    feat(RentalSearchInput): handle input

--- src/components/rental/RentalSearchInput.js
+++ src/components/rental/RentalSearchInput.js
@@ -3,6 +3,25 @@ import { withRouter } from 'react-router-dom';
 
 class RentalSearchInput extends React.Component {
 
+  constructor() {
+    super();
+
+    this.searchInput = React.createRef();
+  }
+
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.handleSearch();
+    }
+  }
+
+  handleSearch() {
+    const { history } = this.props;
+    const city = this.searchInput.current.value;
+
+    city ? history.push(`/rentals/${city}/homes`) : history.push('/rentals');
+  }
+  
   render() {
     return (
       <div className='form-inline my-2 my-lg-0'>
