be17902057188d5890af472a4b3ddbc632a74bc2
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-06 11:08:13 +0100

    feat(BookingManage): display bookings with dynamic data

--- src/components/booking/booking-manage/BookingManage.js
+++ src/components/booking/booking-manage/BookingManage.js
@@ -13,36 +13,35 @@ export class BookingManage extends React.Component {
     const { userBookings } = this.props;
 
     return (
-      <div>
-        {userBookings.map((booking, index) => <p key={index}> {booking.startAt} - {booking.endAt} </p>)}
-        <section id='userBookings'>
-          <h1 className='page-title'>My Bookings</h1>
-          <div className='row'>
+      <section id='userBookings'>
+        <h1 className='page-title'>My Bookings</h1>
+        <div className='row'>
 
-          <div className='col-md-4'>
-              <div className='card text-center'>
-                <div className='card-header'>
-                  Rental Category
-                </div>
-                <div className='card-block'>
-                  <h4 className='card-title'> Rental Title - Rental City</h4>
-                  <p className='card-text booking-desc'>Rental Description</p>
-                  <p className='card-text booking-days'>2018/04/04 - 2018/04/06 | 2 days</p>
-                  <p className='card-text booking-price'><span>Price: </span> <span className='booking-price-value'>205 $</span></p>
-                  <Link className='btn btn-bwm' to='rental detail'>Go to Rental</Link>
-                </div>
-                <div className='card-footer text-muted'>
-                  Created 2018/03/03
-                </div>
+        {userBookings.map((booking, index) =>
+        <div className='col-md-4'>
+            <div className='card text-center'>
+              <div className='card-header'>
+                {booking.rental.category}
+              </div>
+              <div className='card-block'>
+                <h4 className='card-title'> {booking.rental.title} - {booking.rental.city}</h4>
+                <p className='card-text booking-desc'>{booking.rental.description}</p>
+                <p className='card-text booking-days'>{booking.startAt} - {booking.endAt} | {booking.days} days</p>
+                <p className='card-text booking-price'><span>Price: </span> <span className='booking-price-value'>{booking.totalPrice} $</span></p>
+                <Link className='btn btn-bwm' to={`/rentals/${booking.rental.id}`}>Go to Rental</Link>
+              </div>
+              <div className='card-footer text-muted'>
+                Created {booking.createdAt}
               </div>
             </div>
           </div>
-          <div class='alert alert-warning'>
-            You have no bookings created go to rentals section and book your place today.
-            <Link style={{'margin-left': '10px'}} class='btn btn-bwm' to='rentals index page'>Available Rental</Link>
-          </div>
-        </section>
-      </div>
+        )}
+        </div>
+        <div className='alert alert-warning'>
+          You have no bookings created go to rentals section and book your place today.
+          <Link style={{'marginLeft': '10px'}} className='btn btn-bwm' to='rentals index page'>Available Rental</Link>
+        </div>
+      </section>
     )
   }
 }
