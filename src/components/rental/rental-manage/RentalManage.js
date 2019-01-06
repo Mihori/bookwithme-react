b01868c79ba513327807896d13f967cd3ec60e3d
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-06 11:36:03 +0100

    feat(RentalManage): display rentals with hardcoded data

--- src/components/rental/rental-manage/RentalManage.js
+++ src/components/rental/rental-manage/RentalManage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as actions from 'actions';
+import { Link } from 'react-router-dom';
 
 export class RentalManage extends React.Component {
 
@@ -29,6 +30,30 @@ export class RentalManage extends React.Component {
         {
           userRentals.map((rental, index) => <p> {rental.title} </p>)
         }
+        <section id='userRentals'>
+  <h1 className='page-title'>My Rentals</h1>
+  <div className='row'>
+
+    <div className='col-md-4'>
+      <div className='card text-center'>
+        <div className='card-block'>
+          <h4 className='card-title'>Rental Title - Rental City</h4>
+          <Link className='btn btn-bwm' to='rental detail page'>Go to Rental</Link>
+          <button className='btn btn-bwm'> Bookings </button>
+        </div>
+        <div className='card-footer text-muted'>
+          Created at 2018/04/04
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <div className='alert alert-warning'>
+    You dont have any rentals currenty created. If you want advertised your property
+    please follow this link.
+    <Link style={{'marginLeft': '10px'}} className='btn btn-bwm' to='create form'>Register Rental</Link>
+  </div>
+</section>
       </div>
     )
   }
