21475a5a70336b0fecb806c77b119b5fcf75f8d7
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-06 10:44:18 +0100

    feat(RentalManage): add fetch

--- src/components/rental/rental-manage/RentalManage.js
+++ src/components/rental/rental-manage/RentalManage.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import * as actions from 'actions';
 
 export class RentalManage extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      userRentals: [],
+      errors: [],
+      isFetching: false
+    }
+  }
+
+  componentWillMount() {
+    this.setState({isFetching: true});
+
+    actions.getUserRentals().then(
+      userRentals => this.setState({userRentals, isFetching: false}),
+      errors => this.setState({errors, isFetching: false}))
+  }
 
   render() {
     return (
