c6bbe1b4d2ce73e4bb7e94ccd0d75232a07b715e
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-06 10:56:46 +0100

    feat(BookingManage): display booking hardcoded data

--- src/components/booking/booking-manage/BookingManage.js
+++ src/components/booking/booking-manage/BookingManage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import * as actions from 'actions';
 
 export class BookingManage extends React.Component {
@@ -14,6 +15,33 @@ export class BookingManage extends React.Component {
     return (
       <div>
         {userBookings.map((booking, index) => <p key={index}> {booking.startAt} - {booking.endAt} </p>)}
+        <section id='userBookings'>
+          <h1 className='page-title'>My Bookings</h1>
+          <div className='row'>
+
+          <div className='col-md-4'>
+              <div className='card text-center'>
+                <div className='card-header'>
+                  Rental Category
+                </div>
+                <div className='card-block'>
+                  <h4 className='card-title'> Rental Title - Rental City</h4>
+                  <p className='card-text booking-desc'>Rental Description</p>
+                  <p className='card-text booking-days'>2018/04/04 - 2018/04/06 | 2 days</p>
+                  <p className='card-text booking-price'><span>Price: </span> <span className='booking-price-value'>205 $</span></p>
+                  <Link className='btn btn-bwm' to='rental detail'>Go to Rental</Link>
+                </div>
+                <div className='card-footer text-muted'>
+                  Created 2018/03/03
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class='alert alert-warning'>
+            You have no bookings created go to rentals section and book your place today.
+            <Link style={{'margin-left': '10px'}} class='btn btn-bwm' to='rentals index page'>Available Rental</Link>
+          </div>
+        </section>
       </div>
     )
   }
