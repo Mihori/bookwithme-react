4396caab69d8b078df4f3e5f4c0f499115f0a208
Mihori <gabor.dobrozemszky@hotmail.com>
2019-01-06 14:51:58 +0100

    feat(): set environment production conditional to appPath

--- server/index.js
+++ server/index.js
@@ -28,12 +28,14 @@ app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 
+if (process.env.NODE_ENV === 'production') {
   const appPath = path.join(__dirname, '..', 'build');
   app.use(express.static(appPath));
   
   app.get('*', function(req,res) {
     res.sendFile(path.resolve(appPath, 'index.html'))
   });
+}
 
 const PORT = process.env.PORT || 3001;
 
